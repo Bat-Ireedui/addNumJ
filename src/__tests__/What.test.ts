450e2685f88ee18372ac83023f2338a85f7a90d7
dizu00 <86697441+Bat-Ireedui@users.noreply.github.com>
2022-08-22 15:36:05 +0800

    Setup Package

---
+++ src/__tests__/What.test.ts
@@ -0,0 +1,5 @@
+import { addNum } from "../index";
+
+test("5 + 8 should be 13", () => {
+  expect(addNum(5, 8)).toBe(13);
+});
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function addNum(a1: number, a2: number) {
+  return a1 + a2;
+}
